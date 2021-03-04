8e50b43a8c8af9f6fba5a679c8d4aa4bffe72e08
Smarty18 <marty.esp18@gmail.com>
2021-03-04 09:49:32 +0100

    I valori appartengono a [150,300]

---
+++ StampaDecrescenteValori.py
@@ -0,0 +1,8 @@
+#EX: Stampa i valori da 150 a 300 in maniera decrescente
+inizio = int(150)                           #definisco l'inizio dell'intervallo
+fine = int(300)                             #definisco la fine dell'intervallo
+intervallo = fine-inizio
+
+for i in range (0,intervallo+1):            #ciclo sul range
+    print (fine)                            #stampo l'elemento
+    fine-=1                                 #decremento 
