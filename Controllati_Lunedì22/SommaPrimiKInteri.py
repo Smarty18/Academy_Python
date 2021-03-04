70d18734bc6ef1b4b8547cb737bba88340ca77bc
Smarty18 <marty.esp18@gmail.com>
2021-03-04 10:41:09 +0100

    Con inserimento di N -  Non è definita la funzione

--- Controllati_Lunedì22/SommaPrimiKInteri.py
+++ Controllati_Lunedì22/SommaPrimiKInteri.py
@@ -1,4 +1,4 @@
-# programma che riceve in ingresso un numero positivo N e 
+# Programma che riceve in ingresso un numero positivo N e 
 # determina il massimo K tale che òa somma dei primi K interi sia
 # minore o unguale a N 
 # Es. N=20 --> K=5, perchè 1+2+3+4+5=15
@@ -9,14 +9,14 @@ K=0                                              #inizializzo la variabile K (nu
 numero = 1                                       # numero è il successivo che aggiungo al primo numero (nl mio caso il primo numero è somma = 0)
 somma = int(0)                                   # inizializzo la somma
 
-while somma < N :                               #entro nel while finché la somma è minore del valore N
+while somma < N :                                #entro nel while finché la somma è minore del valore N
     
-    somma = somma + numero                      #aggiungo alla somma il successivo (se somma è 0, successivo è 1) 
-    print ("la somma è : ",somma)
-    numero = numero + 1                         # incremento il successivo 
+    somma = somma + numero                       #aggiungo alla somma il successivo (se somma è 0, successivo è 1) 
+    print ("La somma è : ",somma)
+    numero = numero + 1                          # incremento il successivo 
     print (numero)                              
-    if somma < N:                               # voglio che K si incrementi SOLO SE la somma in out è <N
+    if somma < N:                                #voglio che K si incrementi SOLO SE la somma in out è <N
         K = K + 1
-        print("il k è : ",K)
+        print("Il k è : ",K)
      
 print(K)
--- Controllati_Lunedì22/istriangle.py
+++ Controllati_Lunedì22/istriangle.py
@@ -1,10 +1,13 @@
-def isTriangle (a, b, c):
-    if (a+b>=c) and (a+c>=b) and (b+c>=a):
-        if (a+b==c) or (a+c==b) or (b+c==a):
-            print ("degenere")
-        else:
+def isTriangle (a, b, c):                           #Definisco la funzione che richiede
+                                                    #tre input pari alle lunghezze dei
+                                                    #tre lati del triangolo
+    
+    if (a+b>=c) and (a+c>=b) and (b+c>=a):          #condizione affinché siano lati di un triangolo
+        if (a+b==c) or (a+c==b) or (b+c==a):        #condizione affinché sia degenere
+            print ("Degenere")
+        else:                                       #se è triangolo, ma non degenere
             print("Sì")
-    else:
+    else:                                           #se non possono essere lati di un triangolo
         print("No!")
 
-isTriangle(2,3,1)
+isTriangle(2,3,1)                                   #chiamata 
