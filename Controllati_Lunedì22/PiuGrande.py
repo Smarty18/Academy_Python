cc737245b4bd6ae7893dae45ae10d1e280134b37
Smarty18 <marty.esp18@gmail.com>
2021-03-04 10:51:20 +0100

    Stampa del maggiore (funzione con input da tastiera e senza)

--- Controllati_Lunedì22/PiuGrande.py
+++ Controllati_Lunedì22/PiuGrande.py
@@ -1,3 +1,5 @@
+#Funzione che restituisce il maggiore tra due numeri inseriti da tastiera
+
 def piuGrande (numero1,numero2):
     if numero1>numero2:
         print("Il maggiore è:",numero1)
@@ -7,3 +9,13 @@ def piuGrande (numero1,numero2):
 a = input("Inserisci il primo valore: \n")
 b = input("inserisci il secondo valore: \n")
 piuGrande(a,b)
+
+#La stessa funzione può evitare l'input da tastiera (più corretto)
+def piuGrande (numero1,numero2):
+    if numero1>numero2:
+        print("Il maggiore è:",numero1)
+    else :
+        print("Il maggiore è:",numero2)
+
+
+piuGrande(5,7)
