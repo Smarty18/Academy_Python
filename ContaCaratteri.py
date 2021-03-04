29ea1ff8a0fe5adebcca368f0df0a0a7896b600c
Smarty18 <marty.esp18@gmail.com>
2021-03-04 09:52:11 +0100

    La funzione conta il numero di caratteri in una stringa

---
+++ ContaCaratteri.py
@@ -0,0 +1,14 @@
+def contaCaratteri ( ):                                     #definisco la funzione
+    stringa = input("Inserisci una stringa: \n")            #chiedo in input una stringa
+    
+    numCaratteri = 0                                        #inizializzo il numero di caratteri a 0
+                                                    
+    for elem in stringa:                                    #ciclo sulla lunghezza della stringa
+        if elem != " ":                                     #se l'elemento della stringa è diverso dal carattere vuoto
+            numCaratteri += 1                               #aggiorno il numero di caratteri, incrementandolo
+           
+
+    return print("Il numero di caratteri è:",numCaratteri)  #stampa il numero di caratteri e ritorna questo valore alla funzione
+
+
+contaCaratteri()
